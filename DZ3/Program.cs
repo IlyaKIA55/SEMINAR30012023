97c571d455ebd622f57f90f008fb29248585ab46
Ilya <jyg3a125@gmail.com>
2023-01-31 22:06:20 +0500

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int number(string message)
+{
+    Console.WriteLine(message);
+    int numb = Convert.ToInt32(Console.ReadLine());
+    return numb;
+}
+int numberA = number("Введите число от 1 до 7: ");
+
+if (numberA==6 || numberA==7)
+{
+   Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
