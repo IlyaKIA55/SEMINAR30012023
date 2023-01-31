5d184b79e85e10d356410e05ca85d1109637b5ac
Ilya <jyg3a125@gmail.com>
2023-01-31 17:07:38 +0500

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int numb = Convert.ToInt32(Console.ReadLine());
+    return numb;
+}
+
+int numberA = numberRequest("Введите 3х значное число: ");
+
+numberA = (numberA/10) %10;
+
+Console.WriteLine(numberA);
